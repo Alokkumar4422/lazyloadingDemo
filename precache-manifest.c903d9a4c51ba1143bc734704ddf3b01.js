8b6a444bb0ec4996e4db9af9440d27207be99ed4
alok <goyalalok95@gmail.com>
2020-05-05 14:17:32 +0530

    Updates

---
+++ precache-manifest.c903d9a4c51ba1143bc734704ddf3b01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568559e541598f86f4d5aac32523dc77",
+    "url": "/lazyloadingDemo/index.html"
+  },
+  {
+    "revision": "33a89917842a36c09900",
+    "url": "/lazyloadingDemo/static/css/main.24febecc.chunk.css"
+  },
+  {
+    "revision": "f13fdf53013cb441c10c",
+    "url": "/lazyloadingDemo/static/js/2.0857d62a.chunk.js"
+  },
+  {
+    "revision": "33a89917842a36c09900",
+    "url": "/lazyloadingDemo/static/js/main.5039daa0.chunk.js"
+  },
+  {
+    "revision": "695c612880e947c5672c",
+    "url": "/lazyloadingDemo/static/js/runtime~main.a47c24a7.js"
+  }
+]);
